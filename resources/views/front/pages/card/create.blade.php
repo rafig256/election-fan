9f850049cb2ad626a71ad2d707db7c0000a121da
rafig256 <rafig_256@yahoo.com>
2024-06-25 16:51:29 +0330

    add social link

--- resources/views/front/pages/card/create.blade.php
+++ resources/views/front/pages/card/create.blade.php
@@ -5,8 +5,8 @@
     <!--    toastr  -->
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" class="rel">
     <!-- Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <link href="{{asset('front/assets/css/bootstrap.min.css')}}" rel="stylesheet" crossorigin="anonymous">
+    <script src="{{asset('front/assets/js/bootstrap.bundle.min.js')}}" crossorigin="anonymous"></script>
     <title>ثبت کارت هواداری مجازی</title>
     <style>
         @media (max-width: 768px) {
